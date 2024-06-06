4376cf92bb53707229032fcb80fd3785196baee8
DevJoanhac <programadorjh20@gmail.com>
2024-06-06 01:00:42 -0500

    Avance 06/06/2024

---
+++ script.js
@@ -0,0 +1,20 @@
+document.write(`
+<nav class="nav">
+<div class="nav-container">
+    <div>
+        <div class="nav-container-img"><img src="/images/cursor_b.png" alt="Logo de Brawl Stars"></div>
+    </div>
+    <div>
+        <a class="nav-container-a" href="/index.html">Inicio</a>
+    </div>
+    <div>
+        <a class="nav-container-a" href="/htmls/brawlers.html">Brawlers</a>
+    </div>
+    <div >
+        <a class="nav-container-a" href="/htmls/modos_de_juego.html">Modos de Juego</a>
+    </div>
+    <div>
+        <a class="nav-container-a" href="/htmls/competitivo.html">Competitivo</a>
+    </div>
+</div>
+</nav>`)
