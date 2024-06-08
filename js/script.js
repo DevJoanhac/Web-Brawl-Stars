51c42334e95bb741f73636e28c34a64dacab10f5
DevJoanhac <programadorjh20@gmail.com>
2024-06-08 15:35:43 -0500

    Proyecto Terminado

--- js/script.js
+++ js/script.js
@@ -8,13 +8,14 @@ document.write(`
         <a class="nav-container-a" href="/index.html">Inicio</a>
     </div>
     <div>
-        <a class="nav-container-a" href="htmls/brawlers.html">Brawlers</a>
+        <a class="nav-container-a" href="/htmls/brawlers.html">Brawlers</a>
     </div>
     <div >
-        <a class="nav-container-a" href="htmls/modos_de_juego.html">Modos de Juego</a>
+        <a class="nav-container-a" href="/htmls/modos_de_juego.html">Modos de Juego</a>
     </div>
     <div>
-        <a class="nav-container-a" href="htmls/competitivo.html">Competitivo</a>
+        <a class="nav-container-a" href="/htmls/competitivo.html">Competitivo</a>
     </div>
 </div>
 </nav>`)
+
