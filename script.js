6ae26febe6da9ac7b434e8e11fba95417ad8d4ae
DevJoanhac <programadorjh20@gmail.com>
2024-06-07 19:51:40 -0500

    Bonne Nuit!

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ document.write(`
 <nav class="nav">
 <div class="nav-container">
     <div>
-        <div class="nav-container-img"><a href="https://supercell.com/en/games/brawlstars/"><img class="logo-nav" src="/images/logo.png" alt="Logo de Brawl Stars"></a></div>
+        <div class="nav-container-img"><a href="https://supercell.com/en/games/brawlstars/"><img class="logo-nav" src="images/logo.png" alt="Logo de Brawl Stars"></a></div>
     </div>
     <div>
-        <a class="nav-container-a" href="/index.html">Inicio</a>
+        <a class="nav-container-a" href="index.html">Inicio</a>
     </div>
     <div>
-        <a class="nav-container-a" href="/htmls/brawlers.html">Brawlers</a>
+        <a class="nav-container-a" href="htmls/brawlers.html">Brawlers</a>
     </div>
     <div >
-        <a class="nav-container-a" href="/htmls/modos_de_juego.html">Modos de Juego</a>
+        <a class="nav-container-a" href="htmls/modos_de_juego.html">Modos de Juego</a>
     </div>
     <div>
-        <a class="nav-container-a" href="/htmls/competitivo.html">Competitivo</a>
+        <a class="nav-container-a" href="htmls/competitivo.html">Competitivo</a>
     </div>
 </div>
 </nav>`)
