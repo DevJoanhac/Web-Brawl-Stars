3408a60094a1dd09896fd04e2561268566f78f46
DevJoanhac <programadorjh20@gmail.com>
2024-06-08 20:56:48 -0500

    Finalización

--- js/script.js
+++ js/script.js
@@ -1,21 +1,29 @@
 document.write(`
-<nav class="nav">
-<div class="nav-container">
-    <div>
-        <div class="nav-container-img"><a href="https://supercell.com/en/games/brawlstars/"><img class="logo-nav" src="/images/logo.png" alt="Logo de Brawl Stars"></a></div>
+<nav class="main-nav">
+    <div class="logo-nav-container">
+        <div><a href="https://supercell.com/en/games/brawlstars/"><img class="logo-nav" src="/images/logo.png" alt="Logo de Brawl Stars"></a></div>
+        <div class="toogle-main-menu"><i class="fi fi-rr-apps toogle-menu" id="toogle-menu"></i></div>
     </div>
-    <div>
-        <a class="nav-container-a" href="/index.html">Inicio</a>
+    <div id="main-menu" class="main-menu">
+        <div>
+            <a class="main-menu-a" href="/index.html">Inicio</a>
+        </div>
+        <div>
+            <a class="main-menu-a" href="/htmls/brawlers.html">Brawlers</a>
+        </div>
+        <div >
+            <a class="main-menu-a" href="/htmls/modos_de_juego.html">Modos de Juego</a>
+        </div>
+        <div>
+            <a class="main-menu-a" href="/htmls/competitivo.html">Competitivo</a>
+        </div>
     </div>
-    <div>
-        <a class="nav-container-a" href="/htmls/brawlers.html">Brawlers</a>
-    </div>
-    <div >
-        <a class="nav-container-a" href="/htmls/modos_de_juego.html">Modos de Juego</a>
-    </div>
-    <div>
-        <a class="nav-container-a" href="/htmls/competitivo.html">Competitivo</a>
-    </div>
-</div>
-</nav>`)
+</nav>
+`)
 
+const iconMenu = document.querySelector("#toogle-menu");
+const mainMenu = document.querySelector("#main-menu");
+
+iconMenu.addEventListener("click", () => {
+    mainMenu.classList.toggle("main-menu-show");
+})
